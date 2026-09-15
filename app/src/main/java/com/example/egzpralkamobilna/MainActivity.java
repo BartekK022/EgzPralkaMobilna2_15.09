e602b07d4f03a8a2d81ff730cdc7a67430581b8c
BartekK022 <baklu100508@gmail.com>
2026-09-15 14:35:53 +0200

    koniecLekcji

--- app/src/main/java/com/example/egzpralkamobilna/MainActivity.java
+++ app/src/main/java/com/example/egzpralkamobilna/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.egzpralkamobilna;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,17 +12,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnZatwierdz;
+    Button btnWlacz;
+    EditText editTextPobranaLiczba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        editTextPobranaLiczba = findViewById(R.id.editTextText);
+        editTextPobranaLiczba = (int)editTextPobranaLiczba2;
+        btnZatwierdz.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }
+        );
 
     }
-    Button zatwierdz;
-    Button wlacz;
-    EditText pobranaLiczba;
+
+
+
+
 
 
 
