4763ec504dd7afdbcba5ee8d815b6452ac9e8218
BartekK022 <baklu100508@gmail.com>
2026-09-16 10:41:15 +0200

    gotowaFunkcjaPralka

--- app/src/main/java/com/example/egzpralkamobilna/MainActivity.java
+++ app/src/main/java/com/example/egzpralkamobilna/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,19 +16,37 @@ public class MainActivity extends AppCompatActivity {
     Button btnZatwierdz;
     Button btnWlacz;
     EditText editTextPobranaLiczba;
+    TextView textViewPranie;
+    int jakiProgram;
+    TextView textViewOdkurzaczWlaczony;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        editTextPobranaLiczba = findViewById(R.id.editTextText);
-        editTextPobranaLiczba = (int)editTextPobranaLiczba2;
+        editTextPobranaLiczba = findViewById(R.id.editTextNumber);
+        btnZatwierdz = findViewById(R.id.button);
+        textViewPranie = findViewById(R.id.textView3);
+        btnWlacz = findViewById(R.id.button2);
+        textViewOdkurzaczWlaczony = findViewById(R.id.textView5);
+
         btnZatwierdz.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-
+                        jakiProgram = Integer.parseInt(editTextPobranaLiczba.getText().toString());
+                        if(jakiProgram >= 1 && jakiProgram <= 12) {
+                            textViewPranie.setText("Numer prania: " + jakiProgram);
+                        }
+                    }
+                }
+        );
+        btnWlacz.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        textViewOdkurzaczWlaczony.setText("Odkurzacz włączony");
                     }
                 }
         );
